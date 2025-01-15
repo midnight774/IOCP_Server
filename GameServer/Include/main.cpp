07a59a4a6d98e68da36f376a2b78abdae3f47414
midnight774 <midnight774@dongguk.edu>
2025-01-15 16:56:04 +0900

    ADD Initial Folders

---
+++ GameServer/Include/main.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+
+#ifdef _DEBUG
+
+#pragma comment(lib, "SockComm_Debug.lib")
+
+#else
+
+#pragma comment(lib, "SockComm.lib")
+
+#endif // _DEBUG
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
