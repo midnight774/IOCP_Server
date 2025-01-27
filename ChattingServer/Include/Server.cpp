c3bd0813d92ca4c94ee9d989844c4d8d3be077a9
midnight774 <midnight774@dongguk.edu>
2025-01-28 03:22:28 +0900

    Add Process End Signal

--- ChattingServer/Include/Server.cpp
+++ ChattingServer/Include/Server.cpp
@@ -6,17 +6,18 @@
 
 // TODO : TCP 스트림 일부만 송신하고 리턴하는 경우도 고려, 송수신 기능 패킷 매니저로 이관, 클라이언트 관리 기능 세션 매니저로 이관
 
-CServer::CServer() :	m_isStopWorking(false),
-						m_ListenSocket(SocketType::Tcp)
+volatile bool IsStop = false;
+void CloseSignal(int Signal)
 {
-	m_ListenSocket.Bind(Endpoint("0.0.0.0", 5555));
-	
+	if (Signal == SIGINT)
+		IsStop = true;
 }
 
-CServer::CServer(int ThreadCount) : m_isStopWorking(false),
-									m_ListenSocket(SocketType::Tcp),
-									m_ThreadCnt(ThreadCount),
-									m_Iocp(ThreadCount)
+CServer::CServer()
+{
+}
+
+CServer::CServer(int ThreadCount)
 {
 }
 
@@ -50,25 +51,17 @@ void CServer::CloseServer()
 	std::cout << "Chatting Server OFF.\n";
 }
 
-void CServer::EchoChattingData(const char* Data)
-{
-}
-
-void CServer::InitThreadPool()
-{
-	
-}
-
-const int CServer::GetLeastWorkThreadIdx()
-{
-	return 0;
-}
-
 int CServer::Run()
 {
+	signal(SIGINT, CloseSignal);
+
 	try
 	{
-		CPacketIOManager::GetInst()->IocpLoop();
+		while(!IsStop)
+		{
+			CPacketIOManager::GetInst()->IocpLoop();
+		}
+
 		CloseServer();
 	}
 	catch (Exception& e)
--- ChattingServer/Include/Server.h
+++ ChattingServer/Include/Server.h
@@ -10,30 +10,12 @@ public:
 	CServer();
 	CServer(int ThreadCount);
 	~CServer();
-
-private:
-	std::unordered_map<CRemoteClient*, std::shared_ptr<CRemoteClient>> m_mapRemoteClient;
-	Iocp m_Iocp;
-	Socket m_ListenSocket;
-	std::shared_ptr<CRemoteClient> m_PendingClient;
-	bool m_isStopWorking;
-	int m_ThreadCnt;
-
-	std::vector<std::shared_ptr<std::thread>> m_vecThreadPool;
 	
 public:
 	bool InitServer();
 	void CloseServer();
 
 public:
-	void EchoChattingData(const char* Data);
-
-public:
-	void InitThreadPool();
-	const int GetLeastWorkThreadIdx();
-
-public:
 	int Run();
-
 };
 
