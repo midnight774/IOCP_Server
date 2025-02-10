81d4d302cd81a845166539eb16f8eef13bd9ed34
midnight774 <midnight774@dongguk.edu>
2025-02-11 01:52:10 +0900

    ADD Math Libs

---
+++ GameServer/Include/EngineMath.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+
+using namespace DirectX;
+
+#define PI  3.14159f
+
+static float DegreeToRadian(float Angle)
+{
+    return Angle / 180.f * PI;
+}
+
+static float RadianToDegree(float Angle)
+{
+    return Angle * 180.f / PI;
+}
